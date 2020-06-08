445c5a62143ba93f98d21b7198059dba86f7eed1
Joe Nunham <jnunham@estee.com>
2020-06-08 12:21:50 -0400

    Updated terraform.yaml to match CAF-LZ format and removed unused folders

--- modules/azure_region/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "location" {
-  value       = local.regions[var.azure_region]
-  description = "Converted Azure region in DisplayName format"
-}
-
-output "location_short" {
-  value       = local.short_names[var.azure_region]
-  description = "Converted Azure region in short format for resource naming purpose"
-}
