90feb7b44894a9a6c63112e4f41fb968793fd950
Joe Nunham <jnunham@estee.com>
2020-06-08 17:19:42 -0400

    Further commenting out of key vault access policy

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -13,10 +13,10 @@ resource "azurerm_key_vault" "kv" {
  # tenant_id    = data.azurerm_client_config.current.tenant_id
  # object_id    = data.azurerm_client_config.current.object_id
 
-  secret_permissions = [
-    "get",
-    "list",
-    "set",
-    "delete"
-  ]
-}
+  #secret_permissions = [
+  #  "get",
+  #  "list",
+  #  "set",
+  #  "delete"
+  #]
+#}
