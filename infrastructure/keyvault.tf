09563b53fd8f5798ac4e297b753cb39b5eeb3193
Joe Nunham <jnunham@estee.com>
2020-06-08 17:17:18 -0400

    Commented out key vault access policy

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -8,10 +8,10 @@ resource "azurerm_key_vault" "kv" {
   sku_name                    = local.settings.keyvault.sku_name
 }
 
-resource "azurerm_key_vault_access_policy" "admin" {
-  key_vault_id = azurerm_key_vault.example.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+#resource "azurerm_key_vault_access_policy" "admin" {
+ # key_vault_id = azurerm_key_vault.example.id
+ # tenant_id    = data.azurerm_client_config.current.tenant_id
+ # object_id    = data.azurerm_client_config.current.object_id
 
   secret_permissions = [
     "get",
