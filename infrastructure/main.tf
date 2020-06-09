23275594215dcb691e7ce9cc634db2627e8ac1ae
Joe Nunham <jnunham@estee.com>
2020-06-09 10:55:48 -0400

    Moved key vault code to unused folder, updated azurerm provider version to 2.13.0, and switched service connection for AzDo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,15 +4,15 @@ locals {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "jn-eastus2-tfstate-rg"
-    storage_account_name  = "jntfstateeastus2sa"
+    resource_group_name   = "AZ-EUS-TFSTATE-10MDEV-RG"
+    storage_account_name  = "azeastus10mdevtfstate"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  version = "2.11.0"
+  version = "2.13.0"
   features {}
 }
 
@@ -23,7 +23,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "${local.settings.rgPrefix.EUS}-test-${local.settings.rg}"
+resource "azurerm_resource_group" "10mdev" {
+  name     = "${local.settings.rgPrefix.EUS}-10MDEV-${local.settings.rg}"
   location = local.settings.location
 }
