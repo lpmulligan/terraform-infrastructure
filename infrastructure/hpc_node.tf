d98f65b7d0b1f6a161466789575cd728c7e90ec6
Joe Nunham <jnunham@estee.com>
2020-06-09 17:52:18 -0400

    Pulled subnet creation back out of networking.tf and back in to hpc_node.tf

--- infrastructure/hpc_node.tf
+++ infrastructure/hpc_node.tf
@@ -1,3 +1,10 @@
+resource "azurerm_subnet" "subnet" {
+  name                   = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-SN"
+  resource_group_name    = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-${local.settings.rg}"
+  virtual_network_name   = local.settings.vnet.name
+  address_prefixes       = local.settings.vnet.subnet_prefixes
+}
+
 resource "azurerm_network_interface" "interface" {
   count               = local.settings.hpcNode.count
   name                = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-NI-${count.index +1}"
--- infrastructure/unused/networking.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                   = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-SN"
-  resource_group_name    = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-${local.settings.rg}"
-  virtual_network_name   = local.settings.vnet.name
-  address_prefixes       = local.settings.vnet.subnet_prefixes
-}
