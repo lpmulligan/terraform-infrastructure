95aec414808d072f7f20b96a308f09e9f34bcdc7
Joe Nunham <jnunham@estee.com>
2020-06-10 11:10:41 -0400

    Changing vnet data lookup

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,5 +1,1 @@
-data "azurerm_virtual_network" "devvirtualnetwork" {
-  name                = local.settings.vnet.name
-  location            = local.settings.location
-  resource_group_name = local.settings.vnet.rg
-}
+data "azurerm_virtual_network" "devvirtualnetwork" {}
