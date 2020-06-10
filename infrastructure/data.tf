2a85d7cc8d33fe593b539441fe0ba9e060526bc4
Joe Nunham <jnunham@estee.com>
2020-06-10 11:12:37 -0400

    Added back name and resource_group_name for data lookup for vnet

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,1 +1,4 @@
-data "azurerm_virtual_network" "devvirtualnetwork" {}
+data "azurerm_virtual_network" "devvirtualnetwork" {
+  name                = local.settings.vnet.name
+  resource_group_name = local.settings.vnet.rg
+}
