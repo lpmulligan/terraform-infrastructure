b6bc4ecdb1dfc892e9b55fb8bb00b98e082b644c
Joe Nunham <jnunham@estee.com>
2020-06-10 11:08:12 -0400

    Removed locals setting in hpc_node.tf

--- infrastructure/hpc_node.tf
+++ infrastructure/hpc_node.tf
@@ -1,7 +1,3 @@
-locals {
-  settings = yamldecode(file("../environments/10mdeveastus/terraform.yaml"))
-}
-
 resource "azurerm_subnet" "dev10msubnet" {
   name                   = "${local.settings.cloud}-${local.settings.app}-${local.settings.env}-SN"
   resource_group_name    = data.azurerm_virtual_network.devvirtualnetwork.resource_group_name
