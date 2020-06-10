77d6273f416995554d48509900d6be75e6f03c2b
Joe Nunham <jnunham@estee.com>
2020-06-10 11:33:32 -0400

    Commented out vnet lookup

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,4 +1,4 @@
-data "azurerm_virtual_network" "devvirtualnetwork" {
-  name                = local.settings.vnet.name
-  resource_group_name = local.settings.vnet.rg
-}
+#data "azurerm_virtual_network" "devvirtualnetwork" {
+#  name                = local.settings.vnet.name
+#  resource_group_name = local.settings.vnet.rg
+#}
